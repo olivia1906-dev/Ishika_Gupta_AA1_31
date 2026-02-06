20fda9a0f03da2fbf6694ac759254b94c54ca5bc
ISHIKA-GUPTA <ishikag.1906@gmail.com>
2026-02-06 12:01:55 +0530

    MaximumWithoutComparison.java

---
+++ MaximumWithoutComparison.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MaximumWithoutComparison {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int diff = a - b;
+        int k = (diff >> 31) & 1; 
+        int max = a - k * diff;
+
+        System.out.println("Maximum: " + max);
+    }
+}
